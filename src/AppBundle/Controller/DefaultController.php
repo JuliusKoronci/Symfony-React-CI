43a81f2a9e95c527efaf5ece3e74d68caf4d5867
Julius Koronci <jk@web-solutions.sk>
2017-06-03 23:25:59 +0200

    adding linter for config files and twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,11 +12,10 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request, Test $test)
+    public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'test' => $test->test(),
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
