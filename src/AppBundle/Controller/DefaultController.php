9a073c2617cbbb0256c56630cc9ca8fb23d1fefd
Julius Koronci <jk@web-solutions.sk>
2017-06-03 18:12:42 +0200

    Adding phpunit code coverage minimum requirement

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,7 +17,12 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'test' => $test->test(),
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    public function testAction()
+    {
+        return false;
     }
 }
